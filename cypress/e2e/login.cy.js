b0884cfc0572a29d5b00e026b5d051dede046bc4
rrsm <raphael_miranda@apple.com>
2026-05-29 11:30:34 -0300

    update: test bdd login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,11 @@
 describe("Login no sistema Fiscalize", () => {
-  it("deve acessar a tela de login", () => {
+  it("deve acessar a tela de login e fazer login", () => {
     cy.visit("https://projeto-web-one-theta.vercel.app/login");
 
     cy.contains("Fiscalize").should("exist");
     cy.contains("Email").should("exist");
     cy.contains("Senha").should("exist");
     cy.contains("Entrar").should("exist");
-  });
-
-  it("deve fazer login com usuário de teste", () => {
-    cy.visit("https://projeto-web-one-theta.vercel.app/login");
 
     cy.get('input[type="email"]').type("joao@example.com");
     cy.get('input[type="password"]').type("123456");
@@ -18,4 +14,5 @@ describe("Login no sistema Fiscalize", () => {
 
     cy.contains("Meus Chamados").should("exist");
   });
+
 });
